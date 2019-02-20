84870786bbd14f80af62434e3552c8037a59edb1
cgonz0 <cgonzadesign@gmail.com>
2019-02-19 21:44:41 -0800

    commit project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './cherry-picks-logo.svg';
 import './App.css';
+import Infogram from './Infogram';
 
 class App extends Component {
   render() {
@@ -8,18 +9,26 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="intro">
+          <div class="c100 p52 small">
+            <span>OVER</span><span class="percent">50%</span>
+            <div class="slice">
+              <div class="bar"></div>
+              <div class="fill"></div>
+            </div>
+          </div>
+          <span class="stat">of moviegoers<br/>are female</span>
+          <br/>
+          <br/>
+          The critical landscape is still heavily skewed toward white males. Women need a place to go to see how media and entertainment looks through a female lens.
+          <br/>
+          <br/>
+          We’ve converted stars, letter grades, and scores into our 4 prong cherry scoring system. Unscored reviews are carefully assessed by our editorial staff. The unweighted average of all of the individual scores gives us the final cherry score.
+          <br/>
+          <br/>
+          Here's a bit more about how our rating system works:
+        </div>
       </div>
     );
   }
---
+++ src/Infogram.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Infogram extends Component {
+
+  render() {
+    return (
+      <div className="infogram-container">
+        <div className="infogram-embed">Hello</div>
+      </div>
+    )
+  }
+}
+
+export default Infogram;
